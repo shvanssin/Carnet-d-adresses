3bde3de3a033188e98405130af320f87b933225c
Yassine LGARH <Yassine LGARH>
2017-01-07 01:09:46 +0100

    first commit

---
+++ LogManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SingletonPattern
+{
+    public class LogManager
+    {
+        private static LogManager _instance;
+
+        public static LogManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new LogManager();
+
+                return _instance;
+            }
+        }
+        public LogManager() 
+        {
+            _fileStream = File.OpenWrite(GetExecutionFolder() + "\\Application.log");
+            _streamWriter = new StreamWriter(_fileStream);
+        }
+
+        private FileStream _fileStream;
+        private StreamWriter _streamWriter;
+
+        public void WriteLog(string message)
+        {
+            StringBuilder formattedMessage = new StringBuilder();
+            formattedMessage.AppendLine("Date: " + DateTime.Now.ToString());
+            formattedMessage.AppendLine("Message: " + message);
+            _streamWriter.WriteLine(formattedMessage.ToString());
+            _streamWriter.Flush();
+        }
+
+        public string GetExecutionFolder()
+        {
+            return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SingletonPattern
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LogManager.Instance.WriteLog("Test Writing");
+        }
+    }
+}
